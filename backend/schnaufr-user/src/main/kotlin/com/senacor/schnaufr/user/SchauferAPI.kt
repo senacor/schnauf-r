8a6599198a3ae93315c16d1a3a8332506797fdd9
Michael Omann <michael.omann@gmail.com>
2019-06-03 11:51:55 +0100

    add API with rsocket

---
+++ backend/schnaufr-user/src/main/kotlin/com/senacor/schnaufr/user/SchauferAPI.kt
@@ -0,0 +1,63 @@
+package com.senacor.schnaufr.user
+
+import io.reactivex.*
+import io.reactivex.disposables.Disposable
+import io.rsocket.kotlin.*
+import io.rsocket.kotlin.transport.netty.server.*
+import io.rsocket.kotlin.util.AbstractRSocket
+import org.reactivestreams.Publisher
+import org.slf4j.LoggerFactory
+
+class SchnauferAPI {
+
+
+    companion object {
+        val logger = LoggerFactory.getLogger(SchnauferAPI::class.java)
+    }
+
+    private lateinit var disposable: Disposable
+
+    fun setup() {
+        disposable = RSocketFactory
+            .receive()
+            .acceptor { { setup, rSocket -> handler(setup, rSocket) } } // server handler RSocket
+            .transport(TcpServerTransport.create(9090))
+            .start()
+            .subscribe()
+
+    }
+
+    fun stop() {
+        disposable.dispose()
+    }
+
+    fun handler(setup: Setup, rSocket: RSocket): Single<RSocket> {
+        logger.info("received setup {}", setup)
+        return Single.just(MessageHandler())
+    }
+}
+
+class MessageHandler: AbstractRSocket() {
+
+
+    companion object {
+        val logger = LoggerFactory.getLogger(MessageHandler::class.java)
+    }
+
+    override fun requestResponse(payload: Payload): Single<Payload> {
+        logger.info("received payload '{}' with metadata '{}'", payload.dataUtf8, payload.metadataUtf8)
+        return Single.just(DefaultPayload.text("huhu"))
+    }
+
+    override fun fireAndForget(payload: Payload): Completable {
+        return super.fireAndForget(payload)
+    }
+
+    override fun requestChannel(payloads: Publisher<Payload>): Flowable<Payload> {
+        return super.requestChannel(payloads)
+    }
+
+    override fun requestStream(payload: Payload): Flowable<Payload> {
+        return super.requestStream(payload)
+    }
+}
---
+++ backend/schnaufr-user/src/test/kotlin/com/senacor/schnaufr/user/SchnauferAPISpek.kt
@@ -0,0 +1,41 @@
+package com.senacor.schnaufr.user
+
+import io.reactivex.Single
+import io.rsocket.kotlin.*
+import io.rsocket.kotlin.transport.netty.client.TcpClientTransport
+import io.rsocket.kotlin.util.AbstractRSocket
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+class SchnauferAPISpek : Spek({
+
+    describe("schnaufer API") {
+        val schnauferAPI = SchnauferAPI()
+        before {
+            schnauferAPI.setup()
+        }
+
+        after {
+            schnauferAPI.stop()
+        }
+
+        it("should respond with 'huhu'") {
+
+            val rSocket: RSocket = RSocketFactory
+                .connect()
+                .transport(TcpClientTransport.create(9090))
+                .start()
+                .blockingGet()
+
+
+            val response = rSocket.requestResponse(DefaultPayload.text("hi")).blockingGet()
+
+
+            expectThat(response.dataUtf8).isEqualTo("huhu")
+
+        }
+
+    }
+})
