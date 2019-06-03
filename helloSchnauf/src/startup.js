c54e3374423aab27aa0d205ce3ef8174aad3a7ac
Martin Blume <martin.blume@senacor.com>
2019-06-03 10:41:14 +0200

    Review changes

--- helloSchnauf/src/startup.js
+++ helloSchnauf/src/startup.js
@@ -1,23 +1,20 @@
 const config = require('config')
-const http = require('http')
 const { promisify } = require('util')
 const app = require('./app')
 const managementApp = require('./managementApp')
-const port = config.get('Server.port')
-const managementPort = config.get('Server.managementPort')
+const port = config.get('server.port')
+const managementPort = config.get('server.managementPort')
 
 const startServer = async () => {
-  const server = http.createServer(app)
-  await promisify(server.listen.bind(server))(port)
+  await promisify(app.listen.bind(app))(port)
   console.info(`app server running on port ${port}`)
-  return server
+  return app
 }
 
 const startManagementServer = async () => {
-  const managementServer = http.createServer(managementApp)
-  await promisify(managementServer.listen.bind(managementServer))(managementPort)
+  await promisify(managementApp.listen.bind(managementApp))(managementPort)
   console.info(`management server running on port ${managementPort}`)
-  return managementServer
+  return managementApp
 }
 
 const startup = async () => {
