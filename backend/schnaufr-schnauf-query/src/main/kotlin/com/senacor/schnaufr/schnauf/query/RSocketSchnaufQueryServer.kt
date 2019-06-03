8b03e574b678fa05bea2d905c6042c3ca5e95eb5
cersfeld <christoph.ersfeld@senacor.com>
2019-06-03 15:50:42 +0200

    Serialize Schnauf to json

--- backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/RSocketSchnaufQueryServer.kt
+++ backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/RSocketSchnaufQueryServer.kt
@@ -8,14 +8,11 @@ import io.reactivex.disposables.Disposable
 import io.rsocket.kotlin.*
 import io.rsocket.kotlin.transport.netty.server.TcpServerTransport
 import io.rsocket.kotlin.util.AbstractRSocket
-import java.util.*
-import com.squareup.moshi.JsonAdapter
-
 
 
 class RSocketSchnaufQueryServer() {
 
-    private lateinit var disposable : Disposable;
+    private lateinit var disposable: Disposable;
 
     private fun handler(setup: Setup, rSocket: RSocket): Single<RSocket> {
         return Single.just(MessageHandler())
@@ -23,32 +20,29 @@ class RSocketSchnaufQueryServer() {
 
     fun start() {
         disposable = RSocketFactory
-            .receive()
-            .acceptor { { setup, rSocket -> handler(setup, rSocket) } } // server handler RSocket
-            .transport(TcpServerTransport.create(8080))  // Netty websocket transport
-            .start()
-            .subscribe()
+                .receive()
+                .acceptor { { setup, rSocket -> handler(setup, rSocket) } } // server handler RSocket
+                .transport(TcpServerTransport.create(8080))  // Netty websocket transport
+                .start()
+                .subscribe()
     }
 
     fun stop() {
         disposable.dispose()
     }
 
-    private class MessageHandler: AbstractRSocket() {
-
+    private class MessageHandler : AbstractRSocket() {
 
 
         override fun requestStream(payload: Payload): Flowable<Payload> {
             val moshi = Moshi.Builder().build();
             val jsonAdapter = moshi.adapter(Schnauf::class.java)
 
-            Flowable.fromIterable(listOf(Schnauf(UUID(),"christoph", "schnauf" ), Schnauf(UUID(),"mohmann", "schnauf2" )))
-                    .map ( jsonAdapter::toJson )
+
+            return Flowable.fromIterable(listOf(Schnauf(UUID(), "christoph", "schnauf"), Schnauf(UUID(), "mohmann", "schnauf2")))
+                    .map(jsonAdapter::toJson).map { DefaultPayload.text(it) };
 
 
-
-
-            return Flowable.fromIterable(listOf(DefaultPayload.text(), DefaultPayload.text("mohmann-schnauf")))
         }
     }
 }
