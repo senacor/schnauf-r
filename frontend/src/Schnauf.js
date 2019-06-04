f63e10690e1404a942e961b837a04ebdb1fdc3d8
cersfeld <christoph.ersfeld@senacor.com>
2019-06-04 17:16:53 +0200

    First implementtion of rsocket

--- frontend/src/Schnauf.js
+++ frontend/src/Schnauf.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Button from '@material-ui/core/Button'
 
-const Schnauf = ({reason}) => <h1>{reason + '-schnauf'} <Button> Hello World</Button></h1>;
+const Schnauf = ({reason}) => <h1>{reason + '-schnauf'}</h1>;
 
 Schnauf.propTypes = {
   reason: PropTypes.string.isRequired
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import Schnauf from './Schnauf';
 
 import createRSocket from './schnaufRSocket';
 
-createRSocket({url: 'ws://127.0.0.1'}).then((socket) => {
+createRSocket({url: 'ws://127.0.0.1:8080'}).then((socket) => {
   console.log(socket);
 }).catch(console.error)
 
