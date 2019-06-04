35c6e651f7b0cf263d128cda029618d910f57fd5
Neb, Sebastian <Sebastian.Neb@senacor.com>
2019-06-04 11:24:54 +0200

    Added startup for schnauf query

--- backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/SchnaufClient.kt
+++ backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/SchnaufClient.kt
@@ -4,6 +4,8 @@ import com.senacor.schnaufr.schnauf.query.model.Schnauf
 import io.reactivex.Flowable
 
 class SchnaufClient() {
+
+
     fun getAllSchnaufs(): Flowable<Schnauf> {
         return Flowable.empty();
     }
--- backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/SchnaufMessageHandler.kt
+++ backend/schnaufr-schnauf-query/src/main/kotlin/com/senacor/schnaufr/schnauf/query/SchnaufMessageHandler.kt
@@ -5,12 +5,8 @@ import io.rsocket.kotlin.Payload
 import io.rsocket.kotlin.util.AbstractRSocket
 
 class SchnaufMessageHandler(val schnaufClient: SchnaufClient, val schnaufrClient: SchnaufrClient) : AbstractRSocket() {
-
-
     override fun requestStream(payload: Payload): Flowable<Payload> {
         return schnaufClient.getAllSchnaufs()
                 .map { it.asPayload() };
     }
-
-
 }
