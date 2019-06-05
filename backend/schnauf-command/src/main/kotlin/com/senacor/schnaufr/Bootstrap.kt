c0c4763d97f8ba24157ec6997758ff72a7fe58b1
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2019-06-05 11:25:24 +0100

    Tests and refactorings

--- backend/schnauf-command/src/main/kotlin/com/senacor/schnaufr/Bootstrap.kt
+++ backend/schnauf-command/src/main/kotlin/com/senacor/schnaufr/Bootstrap.kt
@@ -1,17 +1,19 @@
 package com.senacor.schnaufr
 
 import com.mongodb.ConnectionString
+import com.senacor.schnaufr.schnauf.MessageHandler
 import com.senacor.schnaufr.schnauf.RSocketServer
 import com.senacor.schnaufr.schnauf.SchnaufRepository
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
 import org.litote.kmongo.reactivestreams.KMongo
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.Executors
 
 object Bootstrap {
 
-    val logger = LoggerFactory.getLogger(Bootstrap::class.java)
+    val logger: Logger = LoggerFactory.getLogger(Bootstrap::class.java)
     var disposable: Disposable? = null
 
     @JvmStatic
@@ -27,11 +29,14 @@ object Bootstrap {
         })
 
         executor.execute {
-            val mongoConnectionString = "mongodb://${System.getenv("MONGO_HOST")}:27017"
+            val mongoHost = System.getenv("MONGO_HOST") ?: "mongo"
+            val mongoConnectionString = "mongodb://$mongoHost:27017"
 
             logger.info("Starting application, connecting to MongoDB at $mongoConnectionString")
+
+            val messageHandler = MessageHandler(SchnaufRepository(KMongo.createClient(ConnectionString(mongoConnectionString))))
             disposable =
-                    RSocketServer(SchnaufRepository(KMongo.createClient(ConnectionString(mongoConnectionString))))
+                    RSocketServer(messageHandler)
                             .start()
                             .subscribeBy { logger.info("Application started") }
 
