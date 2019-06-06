d2d4d4fa31f2aac5f2b9b6749b993bdde3a9d491
Martin Blume <martin.blume@senacor.com>
2019-06-06 12:19:44 +0200

    Added nice momann picture

--- frontend/src/Schnauf.js
+++ frontend/src/Schnauf.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Card} from 'react-bootstrap'
+import { Card, Image, Row , Col} from 'react-bootstrap'
+import picture from '../public/momann.jpeg'
 
 const Schnauf = ({schnauf}) =>
-<Card>
-  <Card.Body>
-    <Card.Title>{schnauf.title}</Card.Title>
-    <Card.Text>
-      {schnauf.author.displayName}
-    </Card.Text>
-  </Card.Body>
-</Card>
+  <Card>
+    <Row>
+      <Col md={2}>
+        <Image src={picture} roundedCircle />
+      </Col>
+      <Col md={{ span: 9, offset: 1 }}>
+        <Card.Body>
+          <Card.Title>{schnauf.title}</Card.Title>
+          <Card.Text>
+            {schnauf.author.displayName}
+          </Card.Text>
+        </Card.Body>
+      </Col>
+
+    </Row>
+  </Card>
 
 Schnauf.propTypes = {
   schnauf: PropTypes.object.isRequired
